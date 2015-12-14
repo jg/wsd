720db2d7075ae1e1231c58bb09b116d60fe50524
bfraczak <b.fraczak@stud.elka.pw.edu.pl>
2015-12-14 22:05:10 +0100

    Hello message exchange

--- src/main/java/com/wsd/wsd_projekt/agents/BatTracker.java
+++ src/main/java/com/wsd/wsd_projekt/agents/BatTracker.java
@@ -1,6 +1,12 @@
 package com.wsd.wsd_projekt.agents;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
+import com.wsd.wsd_projekt.GPSEntry;
 
 import jade.core.AID;
 import jade.core.Agent;
@@ -10,9 +16,34 @@ import jade.lang.acl.ACLMessage;
 import jade.lang.acl.UnreadableException;
 
 public class BatTracker extends Agent {
+	
+	ArrayList<GPSEntry> gps;
+	ArrayList<String> neighbors;
+	float x,y;
+	Random generator;
+	
+	public BatTracker(){
+		generator = new Random();
+		gps = new ArrayList<GPSEntry>();
+		neighbors = new ArrayList<String>();
+		x = generator.nextFloat()*100;
+		y = generator.nextFloat()*100;
+	}
 
 	protected void setup(){
 		System.out.println("TWORZENIE AGENTA");
+		addBehaviour(new TickerBehaviour(this,100) {
+			
+			@Override
+			protected void onTick() {
+				// TODO Auto-generated method stub
+				x += generator.nextFloat()-0.5;
+				y += generator.nextFloat()-0.5;
+				Calendar cal = Calendar.getInstance();
+				Date currentTime = cal.getTime();
+				gps.add(new GPSEntry(x, y, currentTime));
+			}
+		});
 		addBehaviour(new CyclicBehaviour() {
 			
 			@Override
@@ -22,6 +53,13 @@ public class BatTracker extends Agent {
 					System.out.println("DOSTAŁEM");
 					String name = message.getContent();
 					System.out.println("od " + name);
+					if(!neighbors.contains(name)){
+						neighbors.add(name);
+						ACLMessage m = new ACLMessage(ACLMessage.INFORM);
+						m.addReceiver(new AID(name, AID.ISGUID));
+						m.setContent(getAID().getName());;
+						send(m);
+					}
 				}
 				else{
 					block();
@@ -35,7 +73,9 @@ public class BatTracker extends Agent {
 				// TODO Auto-generated method stub
 				System.out.println("BIP "+getAID().getName());
 				ACLMessage m = new ACLMessage(ACLMessage.INFORM);
-				m.addReceiver(new AID("Bat1", AID.ISLOCALNAME));
+				for(int i=0;i<4;i++){
+					m.addReceiver(new AID("Bat"+i+"@192.168.0.12:1099/JADE", AID.ISGUID));
+				}
 				m.setContent(getAID().getName());;
 				send(m);
 			}

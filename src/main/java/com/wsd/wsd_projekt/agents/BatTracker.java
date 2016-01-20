86467e1561ba832758e7c513ed7f7f6c1e6dce94
Juliusz Gonera <juliusz.gonera@gmail.com>
2016-01-20 22:33:52 +0100

    Extract NegotiateDataTransferBehaviour

--- src/main/java/com/wsd/wsd_projekt/agents/BatTracker.java
+++ src/main/java/com/wsd/wsd_projekt/agents/BatTracker.java
@@ -21,6 +21,7 @@ import com.wsd.wsd_projekt.agents.bat_tracker.Battery;
 import com.wsd.wsd_projekt.agents.bat_tracker.SendGpsDataBehaviour;
 import com.wsd.wsd_projekt.agents.bat_tracker.HandleIncomingMessagesBehaviour;
 import com.wsd.wsd_projekt.agents.bat_tracker.SendHelloBehaviour;
+import com.wsd.wsd_projekt.agents.bat_tracker.NegotiateDataTransferBehaviour;
 
 public class BatTracker extends Agent {
 	ArrayList<GPSEntry> gps;
@@ -88,6 +89,10 @@ public class BatTracker extends Agent {
         packages.add(pkg);
     }
 
+    public GPSPackage getPackage(int index) {
+        return packages.get(index);
+    }
+
     public boolean isKnownNeighbour(String name) {
         return neighbors.contains(name);
     }
@@ -109,57 +114,6 @@ public class BatTracker extends Agent {
 		addBehaviour(new SendHelloBehaviour(this, new Long(1000)));
 		
 		//negocjacja przeslania danych
-		addBehaviour(new CyclicBehaviour() {
-			@Override
-			public void action() {
-				MessageTemplate mt = MessageTemplate.or(MessageTemplate.MatchPerformative(ACLMessage.REQUEST), MessageTemplate.MatchPerformative(ACLMessage.AGREE));
-				ACLMessage message = receive(mt);
-				if(message!=null){
-					//jesli otrzymano requesta
-					if(message.getPerformative()==ACLMessage.REQUEST){
-						System.out.println(getAID().getName() + " OTRZYMAŁ REQUEST OD "+message.getSender().getName());
-						ACLMessage reply = message.createReply();
-						//na razie zawsze sie zgadza
-						reply.setPerformative(ACLMessage.AGREE);
-						try {
-							reply.setContentObject(message.getContentObject());
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (UnreadableException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						send(reply);
-					}
-					//jesli otrzymano akceptacje
-					else if(message.getPerformative()==ACLMessage.AGREE){
-						ACLMessage reply = message.createReply();
-						try {
-							Integer index = (Integer)message.getContentObject();
-							reply.setPerformative(ACLMessage.INFORM);
-							try {
-								reply.setContentObject(packages.get(index));
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							//ustaw jezyk na data aby odroznic od Hello
-							reply.setLanguage("data");
-							System.out.println(getAID().getName() + " PRZESYLA DANE DO " + message.getSender().getName());
-							send(reply);
-						} catch (UnreadableException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						
-					}
-						
-				}
-				else{
-					block();
-				}
-			}
-		});
+		addBehaviour(new NegotiateDataTransferBehaviour(this));
 	}
 }
